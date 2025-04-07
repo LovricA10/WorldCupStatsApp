bd9dc4ddb2832ffdaeac9f4200cf2a9c4ca35d28
LovricA10 <lovric.andrej5@gmail.com>
2025-04-07 23:01:10 +0200

    Dao Library

---
+++ Dao/Enums/TypeOfEvent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dao.Enums
+{
+    public enum TypeOfEvent { Goal, GoalOwn, GoalPenalty, RedCard, SubstitutionIn, SubstitutionOut, YellowCard, YellowCardSecond };
+
+}
